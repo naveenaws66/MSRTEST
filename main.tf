28d96c57985d5501789021a0457d209c953ee9f6
naveenaws66 <naveenaws66@gmail.com>
2018-12-20 17:35:46 +0530

    sample test project

---
+++ main.tf
@@ -0,0 +1,24 @@
+variable "accesskey" {
+    type = "string"  
+}
+variable "secrectkey" {
+    type = "string"  
+}
+variable "instancecount" {
+    default = "2"  
+}
+provider "aws" {
+    access_key = "${var.accesskey}"
+    secret_key = "${var.secrectkey}"
+    region = "us-east-1"
+} 
+resource "aws_instance" "ubuntu" {
+    count = "${var.instancecount}"
+    ami = "ami-0f9cf087c1f27d9b1"
+    instance_type = "t2.micro"
+    key_name = "terraform"
+    
+    tags = {
+        Name = "MSR-test-Instance-${count.index+1}"
+    }
+}
---
+++ provisioning.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+sudo apt-get update -y  # updates ubuntu
+sudo apt-get install build-essential -y     #installs required dependencies to install nvm
+#below line install nvm
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+#below line downloads repos to install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+# following line install node
+sudo apt install nodejs -y
+#following lline installs docker
+sudo apt-get install docker.io -y
+#following line downloads and installs required packages to install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#folowing line changes permission to docker-compose folder
+sudo chmod +x /usr/local/bin/docker-compose
+# following line installs openssl
+sudo apt-get install openssl -y
+#following line installs git
+sudo apt-get install git -y
+
+
